93ba7cc4e8c66c3e2d7e60c3426b34f67a02fe6c
Shawn Shacterman <shawnshact@msea.us>
2017-10-14 12:53:48 -0700

    hw1 solutions

---
+++ hw01/hw01.py
@@ -0,0 +1,134 @@
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+
+    if b < 0:
+        f = lambda a,b: sub(a,b)
+    else:
+        f = lambda a,b: add(a,b)
+    return f(a,b)
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return max(a, b)**2 + max(b, c)**2
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    k = n//2
+    def find_factors(n,k):
+        if k == 0:
+            return 1
+        r = n % k
+        if r == 0:
+            return k
+        else:
+            return find_factors(n, k - 2)
+    return find_factors(n,k)
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> with_if_statement()
+    1
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    "*** YOUR CODE HERE ***"
+    c.eval = True
+    return False
+
+def t():
+    "*** YOUR CODE HERE ***"
+    c.eval = False
+    return 0
+
+
+def f():
+    "*** YOUR CODE HERE ***"
+    if c.eval:
+        return 1
+    else:
+        return 0
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    length = 1
+
+    def calcHailstone(n, i):
+        if n == 1:
+            print(int(n))
+            return i
+        elif n % 2 == 0:
+            print(n)
+            return calcHailstone(int(n/2), i+1)
+        else:
+            print(n)
+            return calcHailstone(int(n*3+1), i+1)
+
+    return calcHailstone(n, length)
